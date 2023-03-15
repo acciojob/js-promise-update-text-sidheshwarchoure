d6842734a1097a25d7f023687c83d5747bf7d1b6
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-15 11:02:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateText(params) {
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+	resolve("Hello world!");
+},1000);
+
+});
+}
+
+updateText.then((result)=>{
+	const Output = document.getElementById("output");
+	Output.innerText = result;
+})
+
